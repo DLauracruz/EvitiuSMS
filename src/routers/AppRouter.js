0ee7e1434ca6616361e36de2d3c0cc212474f0dc
Diana Laura Cruz Cruz <laaura060@gmail.com>
2021-03-19 23:02:20 -0600

    Fixed url and router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,43 +1,32 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   HashRouter,
 } from "react-router-dom";
-import { AuthContext } from "../context/auth/AuthContext";
 import { AuthRouter } from "./AuthRouter";
 import { MainRouter } from "./MainRouter";
-import { PrivateRoute } from "./PrivateRoute";
-import { PublicRoute } from "./PublicRoute";
+// import { PrivateRoute } from "./PrivateRoute";
+// import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
-  const { contactsState } = useContext(ContactsContext);
-
-  useEffect(() => {
-    verificaToken();
-  }, [verificaToken]);
-
-  if (auth.checking) {
-    return <h1>Espere por favor</h1>;
-  }
-
   return (
     <HashRouter basename={process.env.PUBLIC_URL}>
       <Router>
         <div>
           <Switch>
             {/* <PublicRoute
-            isAuthenticated={auth.logged}
-            path="/auth"
-            component={AuthRouter}
-          />
-          <PrivateRoute
-            isAuthenticated={auth.logged}
-            exact
-            path="/"
-            component={MainRouter}
-          /> */}
+              isAuthenticated={auth.logged}
+              path="/auth"
+              component={AuthRouter}
+            />
+            <PrivateRoute
+              isAuthenticated={auth.logged}
+              exact
+              path="/"
+              component={MainRouter}
+            /> */}
             <Route path="/auth" component={AuthRouter} />
             <Route path="/" component={MainRouter} />
           </Switch>
